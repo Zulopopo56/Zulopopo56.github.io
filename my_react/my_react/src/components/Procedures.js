e5df4bcbb0f91dbb3ec2c70cb6e438a7bc16efd4
Zulopopo56 <nktklshnk@gmail.com>
2024-08-29 18:09:50 +0300

    Delete my_react directory

--- my_react/my_react/src/components/Procedures.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Procedures.css'
-
-const Procedures = () => {
-    return <div className="procedure">Список процедур</div>;
-  };
-  
-  export default Procedures;
